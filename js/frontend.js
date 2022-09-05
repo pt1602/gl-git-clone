d996176eccbf95780f6ddffc4e34df376da8d25c
Philipp Tuchardt <philipp.tuchardt@nahketing.de>
2022-09-05 07:51:46 +0200

    feat(logic): added typescript support

--- js/frontend.js
+++ js/frontend.js
@@ -1,21 +1,18 @@
+"use strict";
 let cloneButtons = document.querySelectorAll('#ssh_project_clone, #http_project_clone');
 let cloneOptionsDropdown = document.querySelectorAll('.clone-options-dropdown > li > a');
 let mobileCloneButtons = document.querySelector('.mobile-git-clone .clone-dropdown-btn');
 const beforeVal = 'git clone ';
-
 if (cloneButtons) {
     cloneButtons.forEach(function (item) {
-        item.value = beforeVal  + item.value;
+        item.value = beforeVal + item.value;
     });
 }
-
 if (mobileCloneButtons) {
     function modifyMobileCloneButtons() {
         mobileCloneButtons.dataset.clipboardText = beforeVal + mobileCloneButtons.dataset.clipboardText;
     }
-
     modifyMobileCloneButtons();
-
     cloneOptionsDropdown.forEach(item => {
         item.addEventListener('click', modifyMobileCloneButtons);
     });
